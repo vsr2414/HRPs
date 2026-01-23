2a360e4a0b877b2c62272be434801fa243c9a133
vsr2414 <visitha2414@gmail.com>
2026-01-23 23:56:28 +0530

    modified:   Find the runner-up score.py

--- Find the runner-up score.py
+++ Find the runner-up score.py
@@ -25,3 +25,10 @@ Explanation 0
 Given list is . The maximum score is , second maximum is . Hence, we print  as the runner-up score.
 
 """
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    arr = list(set(arr))
+    arr.sort()
+    print(arr[-2])
