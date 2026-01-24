1f36bfbabc6d8bd541462d9098c26d338b83d07f
vsr2414 <visitha2414@gmail.com>
2026-01-24 18:18:41 +0530

    modified:   Find the runner-up score.py Nested lists.py

--- Find the runner-up score.py
+++ Find the runner-up score.py
@@ -27,7 +27,9 @@ Given list is . The maximum score is , second maximum is . Hence, we print  as t
 """
 
 if __name__ == '__main__':
+    
     n = int(input())
+    arr = list(map(int, input().split()))
     arr = list(set(arr))
     arr.sort()
     print(arr[-2])
